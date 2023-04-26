7921489b8430251c87679f217de5d3b7624cb6b6
miggu8066 <pistaker8066@gmail.com>
2023-04-26 12:26:41 +0900

    20230426삭제기능추가

--- board/views.py
+++ board/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import BoardWriteForm
 from .models import Board
 from user.models import User
 from user.decorators import login_required
+
+def board_delete(request, pk):
+    login_session = request.session.get('login_session', '')
+    board = get_object_or_404(Board, id=pk)
+    if board.writer.user_id == login_session:
+        board.delete()
+        return redirect('/board')
+    else:
+        return redirect(f'/board/detail/{pk}/')
+
+
+def board_detail(request, pk):
+    login_session = request.session.get('login_session', '')
+    context = { 'login_session' : login_session }
+
+    board = get_object_or_404(Board, id=pk)
+    context['board'] = board
+
+    if board.writer.user_id == login_session:
+        context['writer'] = True
+    else:
+        context['writer'] = False
+
+    return render(request, 'board/board_detail.html', context)
 
 def board_list(request):
     login_session = request.session.get('login_session', '')
@@ -29,7 +53,7 @@ def board_write(request):
     elif request.method == 'POST':
         write_form = BoardWriteForm(request.POST)
 
-        if write_form.is_vaild():
+        if write_form.is_valid():
             writer = User.objects.get(user_id=login_session)
             board = Board(
                 title = write_form.title,
