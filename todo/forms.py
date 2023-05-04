6ae53d92b56f19aedabfefdfd7da808a66016c68
miggu8066 <pistaker8066@gmail.com>
2023-05-04 17:52:22 +0900

    20230504Todo_list 이해중

--- todo/forms.py
+++ todo/forms.py
@@ -4,8 +4,24 @@ from .models import Todo_list
 class TodoItemsForm(forms.ModelForm):
     class Meta:
         model = Todo_list
-        field = ['todo_content']
+        exclude = ['todo_content']
     
     todo_content = forms.CharField(
-
+        max_length=225,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder' : 'TODAY_ITEM'
+            }
+        ),
     )
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        todo_content = cleaned_data.get('todo_content', '')
+
+        if todo_content == '':
+            self.add_error('todo_content', '내용을 입력하세요')
+        else:
+            self.todo_content = todo_content
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect
+from todo.forms import TodoItemsForm
 from .models import Todo_list
 from user.models import User
+# from user.decorators import login_required
 
+# @login_required
 def todo_main(request):
     login_session = request.session.get('login_session', '')
     context = { 'login_session' : login_session }
 
-    return render(request, "todo/todo_list.html", context)
+    if request.method == 'GET':
+        Itemsform = TodoItemsForm()
+        context['Items'] = Itemsform
+        
+        return render(request, "todo/todo_list.html", context)
