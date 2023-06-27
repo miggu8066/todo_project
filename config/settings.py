1428ff9e302a0f22f3a542ba09f1f151001a685c
miggu8066 <pistaker8066@gamil.com>
2023-06-27 13:16:32 +0900

    20230627배포중에러3

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "NAME": "todo",
         "USER": "root",
         "PASSWORD": "1234",
-        "HOST": "127.0.0.1",
+        "HOST": "hkdev.mysql.pythonanywhere-services.com",
         "PORT": "3306",
     }
 }
