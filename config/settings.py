dc5a810ebd227ac09923f1f69e784eeb92cd481f
miggu8066 <pistaker8066@gamil.com>
2023-06-27 12:23:00 +0900

    20230627배포중2

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "NAME": "todo",
         "USER": "root",
         "PASSWORD": "1234",
-        "HOST": "127.0.0.1",
+        "HOST": "localhost",
         "PORT": "3306",
     }
 }
